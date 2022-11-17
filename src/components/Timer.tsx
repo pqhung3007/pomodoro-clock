07585156bb9c1de9ab4c19e2ec0c473f7dbec237
Quang Hung <hungpham307@gmail.com>
2022-11-17 10:04:40 +0700

    feat: build mock-up countdown timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 function Timer() {
-  return <div>Timer</div>;
+  const renderTime = ({ remainingTime }: { remainingTime: number }) => {
+    if (remainingTime === 0) {
+      return <p>Too late</p>;
+    }
+
+    return (
+      <div className="flex flex-col">
+        <p>Remaining</p>
+        <p>{remainingTime}</p>
+        <p>seconds</p>
+      </div>
+    );
+  };
+
+  return (
+    <CountdownCircleTimer
+      isPlaying
+      duration={10}
+      colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+      colorsTime={[10, 6, 3, 0]}
+      onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+      size={400}
+    >
+      {renderTime}
+    </CountdownCircleTimer>
+  );
 }
 
 export default Timer;
