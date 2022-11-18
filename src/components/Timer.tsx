b347982d681dc4e00b96f62832f47b32dbc3f7e4
Quang Hung <hungpham307@gmail.com>
2022-11-18 11:12:11 +0700

    style: edit clock width based on screen size

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -10,17 +10,35 @@ function Timer() {
   const getDurationByMode = convertMinutesToSeconds(+timerMode[mode]);
 
   return (
-    <CountdownCircleTimer
-      isPlaying={isPlaying}
-      key={resetKey}
-      duration={getDurationByMode}
-      colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-      colorsTime={[10, 6, 3, 0]}
-      onComplete={() => ({ shouldRepeat: true, delay: 1 })}
-      size={400}
-    >
-      {({ remainingTime }) => <TimerInner remainingTime={remainingTime} />}
-    </CountdownCircleTimer>
+    <>
+      <div className="hidden md:block">
+        <CountdownCircleTimer
+          isPlaying={isPlaying}
+          key={resetKey}
+          duration={getDurationByMode}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[10, 6, 3, 0]}
+          onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+          size={400}
+        >
+          {({ remainingTime }) => <TimerInner remainingTime={remainingTime} />}
+        </CountdownCircleTimer>
+      </div>
+
+      <div className="block md:hidden">
+        <CountdownCircleTimer
+          isPlaying={isPlaying}
+          key={resetKey}
+          duration={getDurationByMode}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[10, 6, 3, 0]}
+          onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+          size={330}
+        >
+          {({ remainingTime }) => <TimerInner remainingTime={remainingTime} />}
+        </CountdownCircleTimer>
+      </div>
+    </>
   );
 }
 
